931c6992fbc7fb7b991f59c89cfa94897643129e
davadhut99 <davadhut.99@gmail.com>
2022-11-23 19:18:11 +0530

    currying task_09

--- advanced_js/task_09.js
+++ advanced_js/task_09.js
@@ -16,13 +16,44 @@ console.log(bound(1, 2, 3));
 // Create a new object called Student with age 20 , write a function which prints
 // the age of the student from the student object. Don't pass in args. Read from 'this' and use BIND
 
-var student = { age: 20,
-printage:function(){
-    return this.age;
-}}
+var student = {
+    age: 20,
+    printage: function () {
+        return this.age;
+    }
+}
 
 
-const ashish= {age:30};
+const ashish = { age: 30 };
 
 let ashishAge = student.printage.bind(student);
 console.log(ashishAge());
+
+
+
+
+//Currying
+//by bind() method
+// let multiply =function(x,y){
+//     console.log(x*y);
+// }
+
+// let multiplyByTwo = multiply.bind(this,2);
+// multiplyByTwo(3);
+
+// let multiplyByThree=multiply.bind(this,3);
+// multiplyByThree(5);
+
+
+// by using closure
+
+let multiply = function (x) {
+    return function (y) {
+        console.log(x * y);
+    }
+}
+
+let multiplyByTwo = multiply(2);
+multiplyByTwo(3);
+let multiplyByThree = multiply(3);
+multiplyByThree(5);
