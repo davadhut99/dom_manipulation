8e4444e515a04f1031dd1dade559d7fe078c438c
davadhut99 <davadhut.99@gmail.com>
2022-12-10 21:56:04 +0530

    expense_tracker_done ---)

--- expense_tracker_integration/expense10.js
+++ expense_tracker_integration/expense10.js
@@ -3,7 +3,7 @@ const description = document.getElementById("description");
 const category = document.getElementById("category");
 const addExpnenseButton = document.getElementById("addexpense");
 //const parentNode = document.getElementById("listOfExpences");
-
+let editingId = null;
 addExpnenseButton.addEventListener("click", function (e) {
   e.preventDefault();
   const expamount = expenseAmount.value;
@@ -14,29 +14,53 @@ addExpnenseButton.addEventListener("click", function (e) {
     expdec,
     expcat,
   };
-  
+
   console.log(obj);
-  axios
-    .post(
-      "https://crudcrud.com/api/1efc3c1c099f4497b2bbd94b6da310f9/expencetracker",
-      obj
-    )
-    .then((response) => {
-      console.log(response);
-      showNewExpencesOnScreen(response.data);
-      console.log(response);
-    })
-    .catch((err) => {
-      document.body.innerHTML =
-        document.body.innerHTML + "<h4> Something went wrong! </h4>";
-      console.log(err);
-    });
+
+  if (editingId === null) {
+    axios
+      .post(
+        "https://crudcrud.com/api/e373538d7e4f45199ceaa3cbd98f7aaf/expencetracker",
+        obj
+      )
+      .then((response) => {
+        console.log(response);
+        showNewExpencesOnScreen(response.data);
+        console.log(response);
+      })
+      .catch((err) => {
+        document.body.innerHTML =
+          document.body.innerHTML + "<h4> Something went wrong! </h4>";
+        console.log(err);
+      });
+  }
+  else {
+    axios
+      .put(
+        `https://crudcrud.com/api/e373538d7e4f45199ceaa3cbd98f7aaf/expencetracker/${editingId}`, obj
+      )
+      .then((response) => {
+        console.log(response);
+        showNewExpencesOnScreen(obj);
+        console.log(response);
+      })
+      .catch((err) => {
+        document.body.innerHTML =
+          document.body.innerHTML + "<h4> Something went wrong! </h4>";
+        console.log(err);
+      });
+    editingId = null;
+  }
+
+
+
+
 });
 
 window.addEventListener("DOMContentLoaded", () => {
   axios
     .get(
-      "https://crudcrud.com/api/1efc3c1c099f4497b2bbd94b6da310f9/expencetracker"
+      "https://crudcrud.com/api/e373538d7e4f45199ceaa3cbd98f7aaf/expencetracker"
     )
     .then((response) => {
       console.log(response);
@@ -60,49 +84,30 @@ function showNewExpencesOnScreen(ele) {
   }
   const parentNode = document.getElementById("listOfExpences");
   childHTML = `<li id=${ele._id}> ${ele.expamount} - ${ele.expdec} - ${ele.expcat}
-    <button onClick= editExpenceDetails('${ele.expamount}','${ele.expdec}','${ele.expcat}','${ele._id}')> Edit </button>
+    <button onClick="editExpenceDetails('${ele.expamount}','${ele.expdec}','${ele.expcat}','${ele._id}')"> Edit </button>
     <button onclick= deleteExpence('${ele._id}')> Delete </button></li>`;
 
   parentNode.innerHTML = parentNode.innerHTML + childHTML;
 }
 
 function editExpenceDetails(expamount, expdec, expcat, eleId) {
-  var editingId = null;
   document.getElementById("expenseAmount").value = expamount;
   document.getElementById("description").value = expdec;
   document.getElementById("category").value = expcat;
 
   editingId = eleId;
+  console.log(eleId);
   removeExpenceFromScreen(eleId);
-  deleteExpence(eleId);
+  //deleteExpence(eleId);
 
-  function handleSubmit() {
-    if (editingId === null) {
-      axios
-        .post(
-          `https://crudcrud.com/api/1efc3c1c099f4497b2bbd94b6da310f9/expencetracker/${editingId}`
-        )
-        .then((res) => showNewExpencesOnScreen(ele))
-    }
-    else {
-      axios
-        .put(
-          `https://crudcrud.com/api/1efc3c1c099f4497b2bbd94b6da310f9/expencetracker/${editingId}`
-        )
-        .then((res) => {
-          showNewExpencesOnScreen(ele);
-        })
-    }
-    editingId = null;
-  }
-  handleSubmit();
 }
+
 
 
 function deleteExpence(eleId) {
   axios
     .delete(
-      `https://crudcrud.com/api/1efc3c1c099f4497b2bbd94b6da310f9/expencetracker/${eleId}`
+      `https://crudcrud.com/api/e373538d7e4f45199ceaa3cbd98f7aaf/expencetracker/${eleId}`
     )
     .then((response) => {
       console.log(response);
@@ -120,3 +125,6 @@ function removeExpenceFromScreen(eleId) {
     parentNode.removeChild(childNodeToBeDeleted);
   }
 }
+
+
+//https://crudcrud.com/api/e373538d7e4f45199ceaa3cbd98f7aaf
